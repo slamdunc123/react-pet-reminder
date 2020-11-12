14e2bc593346f9582e200d8abfac13d403d82983
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-11-12 11:02:53 +0000

    create simple welcome message for logged in and registered user

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
-	return <div className='container'></div>;
+	const { isAuthenticated, user } = useSelector((state) => state.authReducer);
+	return (
+		<div className='container'>
+			<h1>
+				{isAuthenticated
+					? `Welcome ${user && user.name}`
+					: 'Please login or register'}
+			</h1>
+		</div>
+	);
 };
 
 export default Home;
