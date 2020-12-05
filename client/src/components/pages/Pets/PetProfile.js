35f259a714770d0ac93e87e9e9f3742dcf2e83e6
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-12-05 21:59:46 +0000

    add pet edit functionality using button and modal form

--- client/src/components/pages/Pets/PetProfile.js
+++ client/src/components/pages/Pets/PetProfile.js
@@ -1,37 +1,119 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import PetsForm from './PetsForm';
+import Modal from '../../partials/Modal/Modal';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import {
+	getPets,
+	deletePet,
+	updatePet,
+} from '../../../redux/actions/petActions';
+import { resetAlerts } from '../../../redux/actions/alertActions';
 
 const PetProfile = () => {
+	const dispatch = useDispatch();
 	const pets = useSelector((state) => state.petReducer.pets); //TODO: need to fix refesh issue where pets reverts back to an empty array
+	const alerts = useSelector((state) => state.alertReducer);
+	const [showModal, setShowModal] = useState(false);
+	const [modalTitle, setModalTitle] = useState('');
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedPet, setEditedPet] = useState({
+		id: '',
+		name: '',
+		desc: '',
+	});
+	const [updatedPet, setUpdatedPet] = useState(false);
+	const [petId, setPetId] = useState();
+
 	const history = useHistory();
 	const { location: pathname } = history;
 	const pathUrl = pathname.pathname;
-
 	const pathUrlLastItem = pathUrl.substring(pathUrl.lastIndexOf('/') + 1);
-
 	const pet = pets.find((pet) => pet._id === pathUrlLastItem);
-
 	const { _id, name, desc } = pet;
+
+	const getUserId = () => {
+		const userId = localStorage.getItem('userId');
+		return userId;
+	};
+
+	const handleUpdate = (id, formData) => {
+		setShowModal(false);
+		setIsEditing(false);
+		setUpdatedPet(true);
+		dispatch(updatePet(id, formData));
+	};
+
+	const handleEdit = (id, name, desc) => {
+		setShowModal(true);
+		setModalTitle('edit');
+		setIsEditing(true);
+		setEditedPet({
+			id: id,
+			name: name,
+			desc: desc,
+		});
+	};
+
+	const handleDelete = () => {
+		setShowModal(false);
+		dispatch(deletePet(petId));
+	};
+
+	const getModalBody = () => {
+		return modalTitle === 'delete' ? (
+			<>
+				<h3>Are you sure?</h3>
+				<hr />
+				<button className='btn btn-danger' onClick={handleDelete}>
+					Delete
+				</button>
+			</>
+		) : (
+			<PetsForm
+				isEditing={isEditing}
+				editedPet={editedPet}
+				handleUpdate={handleUpdate}
+			/>
+		);
+	};
+
+	const getModal = () => {
+		return (
+			<Modal
+				title={modalTitle}
+				body={getModalBody()}
+				setShowModal={setShowModal}
+			/>
+		);
+	};
+
+	useEffect(() => {
+		dispatch(resetAlerts());
+		dispatch(getPets(getUserId()));
+		setUpdatedPet(false);
+	}, [updatedPet, dispatch]);
+
 	return (
 		<>
 			<div>Pet Profile</div>
+			{showModal ? getModal() : false}
 			<div className='card'>
 				<div className='text-center'>
 					<i className='fas fa-paw fa-3x text-primary'></i>
 				</div>
 				<div className='card-body'>
-					<h5 className='card-title text-center'>{pet.name}</h5>
-					<p className='card-text'>{pet.desc}</p>
+					<h5 className='card-title text-center'>{name}</h5>
+					<p className='card-text'>{desc}</p>
 					<div className='row justify-content-center'>
-						{/* <button
+						<button
 							onClick={() => handleEdit(_id, name, desc)}
 							className='btn'
 							disabled={alerts.length > 0}
 						>
 							<i className='fas fa-pencil-alt text-warning'></i>
 						</button>
-						<button
+						{/* <button
 							onClick={() => handleRemove(_id)}
 							className='btn'
 							disabled={alerts.length > 0}
