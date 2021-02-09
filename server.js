64dd2518df80841d848ecf8bd5cc067ca3ad923f
duncanlaidlaw1 <slamdunc123@gmail.com>
2021-02-09 20:53:31 +0000

    add env file for environment variables

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const conn = require('./config/db');
 const path = require('path');
 
-const config = require('config');
-const accountSid = config.get('TWILIO_ACCOUNT_SID');
-const authToken = config.get('TWILIO_AUTH_TOKEN');
+// const config = require('config');
+// const accountSid = config.get('TWILIO_ACCOUNT_SID');
+// const authToken = config.get('TWILIO_AUTH_TOKEN');
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 const app = express();
