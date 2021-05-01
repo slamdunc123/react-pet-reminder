64a65e611a93255241e96ec3c500725ca1324bf1
duncanlaidlaw <slamdunc123@gmail.com>
2021-05-01 20:21:32 +0100

    pet record styling

--- client/src/components/pages/Pets/PetRecord.js
+++ client/src/components/pages/Pets/PetRecord.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './pets.scss';
 
 const PetRecord = ({ pet }) => {
 	const alerts = useSelector((state) => state.alertReducer);
