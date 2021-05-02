6ee409b431b073dc336ea8a56756187d56a81cfb
duncanlaidlaw <slamdunc123@gmail.com>
2021-05-02 23:06:53 +0100

    clear localStorage on refresh and tab close

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/layout/Header/Header';
 import Main from './components/layout/Main/Main';
@@ -9,6 +9,15 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 function App() {
+	useEffect(() => {
+		window.addEventListener('beforeunload', () => localStorage.clear());
+		return () => {
+			window.removeEventListener('beforeunload', () =>
+				localStorage.clear()
+			);
+		};
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Router>
